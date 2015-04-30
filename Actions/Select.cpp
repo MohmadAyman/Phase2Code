4c940c7ff87a970b006c8f459841e4045ccb1dd7
Mohmmad Ayman <no.future2e1@gmail.com>
2015-04-30 14:52:17 +0200

    tracked

---
+++ Actions/Select.cpp
@@ -0,0 +1,47 @@
+#include "Select.h"
+#include "..\ApplicationManager.h"
+#include "..\Move.h"
+#include "..\GUI\input.h"
+#include "..\GUI\Output.h"
+
+Select::Select(ApplicationManager *pAppManager):Action(pAppManager)
+{}
+Select::~Select()
+{}
+void Select::ReadActionParameters()
+{
+	Input *pIn = pManager->GetInput();
+	Output *pOut = pManager->GetOutput();
+
+	pOut->PrintMessage("you selected a statment");
+
+	pIn->GetPointClicked(Position);
+	pOut->ClearStatusBar();
+}
+
+void Select::Execute()
+{
+	ReadActionParameters();
+	Input *pIn = pManager->GetInput();
+	Output *pOut = pManager->GetOutput();
+	pIn->GetPointClicked(Position);
+	Statement *s=pManager->GetStatement(Position);
+	if (s != nullptr)
+	{
+
+		if (s->IsSelected() != true)
+		{
+			pManager->SetSelectedStatement(s);
+			s->SetSelected(true);
+			pOut->PrintMessage("you selected a statment");
+			if (buttonstate() == 1) {
+				//s->Move();
+			}
+		}
+		else
+		{
+			s->SetSelected(false);
+			pOut->PrintMessage("you Unselected a statment");
+		}
+	}
+}
